3b9cbc44a95c6fd741e5e3daafbb0007781686aa
JoEunsoo <dcpg95@gmail.com>
2022-08-22 21:59:01 +0900

    btn click counting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+/* eslint-disable prettier/prettier */
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   const [className, setClassName] = useState(true);
+  const [counts, setCount] = useState(0);
   const changeName = () => {
     setClassName(!className);
+  };
+
+  const btnCount = () => {
+    setCount(counts + 1);
   };
 
   return (
@@ -13,6 +19,8 @@ function App() {
       <header className={className ? 'App-header' : 'App-header2'}>
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={changeName}>백그라운드변경</button>
+        <button className="btn" onClick={btnCount}> 카운팅 </button>
+        <div>{counts}</div>
       </header>
     </div>
   );
